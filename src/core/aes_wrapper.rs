ec618d1f25f29d8f3ac88f3ecca9e770a78ff23a
Anoncomrade993 <ifeanyiotah001@gmail.com>
2024-05-05 08:09:39 +0100

    update

--- src/core/aes_wrapper.rs
+++ src/core/aes_wrapper.rs
@@ -53,3 +53,38 @@ pub fn decrypt(pixels: &mut Vec<Vec<u8>>, key: &str) -> Result<Vec<Vec<u8>>, io:
 
     Ok(pixels.clone()) // Return the decrypted pixels
 }
+
+
+pub fn test(pixels:&mut Vec<Vec<u8>>){
+    println!("init {:?}",pixels);
+    
+    //AddRoundKey
+    //subBytes
+    //shift_rows
+    //MixColumn
+    let fess = pixels.clone();
+    let mut key =b"";
+    aes::add_round_key(pixels,&mut key);
+    println!("add_round_key {:?}",pixels);
+    aes::sub_pixels(pixels);
+    println!("sub_pixelss {:?}",pixels);
+    aes::shift_rows(pixels);
+    println!("shift_rows {:?}",pixels);
+    aes::mix_columns(pixels);
+    println!("mix_columns {:?}",pixels);
+    
+    //inverse MixColumn
+    //inverse shift_rows
+    //inverse subBytes
+    //AddRoundKey 
+    aes::inv_mix_columns(pixels);
+    println!("inv_mix_columns {:?}",pixels);
+    aes::inv_shift_rows(pixels);
+    println!("inv_shift_rows {:?}",pixels);
+    aes::inv_sub_pixels(pixels);
+    println!("inv_sub_pixels {:?}",pixels);
+    aes::add_round_key(pixels,&mut key);
+    println!("add_round_key {:?}",pixels);
+    let lass = pixels.clone();
+    assert_eq!(fess,lass);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-mod core;
-use crate::core::aes_wrapper::{encrypt, decrypt};
-use crate::core::image::Image;
 #[allow(dead_code)]
 #[allow(unused_imports)]
+mod core;
+use crate::core::aes_wrapper::{encrypt, decrypt,test};
+use crate::core::image::Image;
+
+
 fn main() {
     let s16 = "src/assets/subject_16_by16.jpg";
     let _s32 = "src/assets/subject_32_by_32.jpg";
@@ -12,7 +14,7 @@ fn main() {
     let save: Option<String> = Some(String::from("src/assets/test.png"));
     let dims: Option<(usize, usize)> = None;
     let pixel: Option<Vec<Vec<u8>>> = None;
-    let key: &str = "ILoveRustSoMuch!";
+    let _key: &str = "ILoveRustSoMuch!";
 
     let image = Image::new(src, save, dims, pixel);
     let img = image.read_image().unwrap();
@@ -20,8 +22,9 @@ fn main() {
     let mut data: Vec<Vec<u8>> = img.pixels.clone().unwrap();
     let (width, height) = img.dims.clone().unwrap();
 
-    let mut enc = encrypt(&mut data, key).unwrap();
-    println!("{:?}", &enc[0..4]);
-    let _= image.write_image(&mut enc, width, height);
+    test(data);
+   // let mut enc = encrypt(&mut data, key).unwrap();
+   //println!("{:?}", &enc[0..4]);
+   //let _= image.write_image(&mut enc, width, height);
     
 }
